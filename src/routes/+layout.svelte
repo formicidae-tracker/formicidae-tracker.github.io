be9c645e48a1159e051617b4f213f1e51a5c9815
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2023-08-03 11:27:02 +0200

    Base templating with skeleton

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+	import '../app.postcss';
+
+	import { setInitialClassState } from '@skeletonlabs/skeleton';
+	import LightSwitch from '$lib/utilities/LightSwitch.svelte';
+</script>
+
+<svelte:head>{@html `<script>(${setInitialClassState.toString()})();</script>`}</svelte:head>
+<LightSwitch />
+<slot />
