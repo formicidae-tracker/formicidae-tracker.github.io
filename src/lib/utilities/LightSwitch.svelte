0b5615bf872c845e0eb882c9bd60d55872521b67
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2023-08-03 14:45:41 +0200

    Sets a logo and a custom theme.

--- src/lib/utilities/LightSwitch.svelte
+++ src/lib/utilities/LightSwitch.svelte
@@ -3,11 +3,19 @@
 </script>
 
 {#if $modeCurrent}
-	<button type="button" class="btn-icon" on:click={() => setModeCurrent(false)}>
+	<button
+		type="button"
+		class="btn-icon hover:variant-soft-primary"
+		on:click={() => setModeCurrent(false)}
+	>
 		<span class="material-icons-outlined">dark_mode</span>
 	</button>
 {:else}
-	<button type="button" class="btn-icon" on:click={() => setModeCurrent(true)}>
+	<button
+		type="button"
+		class="btn-icon hover:variant-soft-primary"
+		on:click={() => setModeCurrent(true)}
+	>
 		<span class="material-icons-outlined">light_mode</span>
 	</button>
 {/if}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,11 @@
 <script>
-	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+	import '../theme.postcss';
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	import '../app.postcss';
 
 	import { AppBar, AppShell, setInitialClassState } from '@skeletonlabs/skeleton';
 	import LightSwitch from '$lib/utilities/LightSwitch.svelte';
+	import Logo from '$lib/components/Logo.svelte';
 </script>
 
 <svelte:head>{@html `<script>(${setInitialClassState.toString()})();</script>`}</svelte:head>
@@ -12,9 +13,7 @@
 	<svelte:fragment slot="header">
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<button type="button" class="btn-icon">
-					<span class="material-icons-outlined">menu</span>
-				</button>
+				<Logo />
 			</svelte:fragment>
 			<h2 class="h2">FORmicidae Tracker</h2>
 			<svelte:fragment slot="trail">
