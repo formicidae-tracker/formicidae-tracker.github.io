15386cb7bdee349052abe8a82de122192969ae5d
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2023-08-03 12:00:22 +0200

    Basic skeleton layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,10 +3,25 @@
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	import '../app.postcss';
 
-	import { setInitialClassState } from '@skeletonlabs/skeleton';
+	import { AppBar, AppShell, setInitialClassState } from '@skeletonlabs/skeleton';
 	import LightSwitch from '$lib/utilities/LightSwitch.svelte';
 </script>
 
 <svelte:head>{@html `<script>(${setInitialClassState.toString()})();</script>`}</svelte:head>
-<LightSwitch />
-<slot />
+<AppShell>
+	<svelte:fragment slot="header">
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<button type="button" class="btn-icon">
+					<span class="material-icons-outlined">menu</span>
+				</button>
+			</svelte:fragment>
+			<h2 class="h2">FORmicidae Tracker</h2>
+			<svelte:fragment slot="trail">
+				<LightSwitch />
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<slot />
+	<svelte:fragment slot="footer">footer</svelte:fragment>
+</AppShell>
