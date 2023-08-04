2bd3afe92924fff792d6df33932c65052f7a69fe
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2023-08-04 17:08:31 +0200

    Fixes the light switch

--- src/lib/utilities/LightSwitch.svelte
+++ src/lib/utilities/LightSwitch.svelte
@@ -1,21 +1,31 @@
 <script lang="ts">
-	import { modeCurrent, setModeCurrent } from '@skeletonlabs/skeleton';
+	import {
+		getModeOsPrefers,
+		modeCurrent,
+		setModeCurrent,
+		setModeUserPrefers
+	} from '@skeletonlabs/skeleton';
+	import { onMount } from 'svelte';
+
+	function toggleMode() {
+		$modeCurrent = !$modeCurrent;
+		setModeCurrent($modeCurrent);
+		setModeUserPrefers($modeCurrent);
+	}
+
+	onMount(() => {
+		if (!('modeCurrent' in localStorage)) {
+			setModeCurrent(getModeOsPrefers());
+		}
+	});
 </script>
 
 {#if $modeCurrent}
-	<button
-		type="button"
-		class="btn-icon hover:variant-soft-primary"
-		on:click={() => setModeCurrent(false)}
-	>
+	<button type="button" class="btn-icon hover:variant-soft-primary" on:click={toggleMode}>
 		<span class="material-icons-outlined">dark_mode</span>
 	</button>
 {:else}
-	<button
-		type="button"
-		class="btn-icon hover:variant-soft-primary"
-		on:click={() => setModeCurrent(true)}
-	>
+	<button type="button" class="btn-icon hover:variant-soft-primary" on:click={toggleMode}>
 		<span class="material-icons-outlined">light_mode</span>
 	</button>
 {/if}
