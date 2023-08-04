6fc464cc550689ddf47c50e84c5a16c658edf652
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2023-08-04 14:24:49 +0200

    Redirects everything to github wiki

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,8 +1,8 @@
+import { redirect } from '@sveltejs/kit';
 import type { LayoutLoad } from './$types';
 
 export const prerender = true;
 
-export const load: LayoutLoad = ({ url }) => {
-	console.log(url.origin);
-	return {};
+export const load: LayoutLoad = () => {
+	throw redirect(302, 'https://github.com/formicidae-tracker/documentation/wiki');
 };
