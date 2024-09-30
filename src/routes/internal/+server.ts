f7d13f6991e7f2dfb7d3d346c22eec046659ba63
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2024-09-30 12:22:28 +0200

    WIP

--- src/routes/internal/+server.ts
+++ src/routes/internal/+server.ts
@@ -1,4 +1,4 @@
-import { buildSitemap } from '$lib/utilities/fetchPages';
+import { buildSitemap } from '$lib/utilities/buildSitemap';
 import { json, type RequestHandler } from '@sveltejs/kit';
 
 export const GET: RequestHandler = async ({}) => {
